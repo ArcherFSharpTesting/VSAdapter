680867f42ca4b56df9e7cc78ca951d6d3af99b6c
jason-kerney <jason@teamkerney.com>
2023-07-03 16:34:39 -0700

    chore: figguring things out

---
+++ Dummy.Tests/FizzBuzz Should.fs
@@ -0,0 +1,28 @@
+﻿module Dummy.Tests.``FizzBuzz Should``
+
+open Archer
+open Archer.Arrows
+open Archer.Arrows.Internals
+open Archer.Arrows.Internal.Types
+
+let private feature = Arrow.NewFeature ()
+
+type private Thing = {
+    UnitProp: unit
+}
+
+let private names =
+    let t = typeof<Thing>
+    t.Namespace, t.DeclaringType.Name
+    
+let FizzBuzz value = ""
+
+let ``Convert 1 to "1"`` =
+    feature.Test (fun _ ->
+        1
+        |> FizzBuzz
+        |> Should.BeEqualTo "1"
+    )
+
+
+// let ``Test Cases`` = feature.GetTests ()
--- Lib/TestGetter.fs
+++ Lib/TestGetter.fs
@@ -2,23 +2,54 @@
 
 open System
 open System.IO
+open System.Reflection
+open Archer.CoreTypes.InternalTypes
 open Microsoft.VisualStudio.TestPlatform.ObjectModel
 
 let getPath (source: string) =
-    if Path.IsPathRooted source then source
+    if Path.IsPathRooted source then source |> FileInfo
     else
         (Directory.GetCurrentDirectory (), source)
         |> Path.Combine
+        |> FileInfo
         
 let getTests (sources: string seq) =
     sources
-        |> Seq.mapi (fun i name ->
-            let path = getPath name
-            let fns =
-                name.Split ([|"."; "\\"|], StringSplitOptions.RemoveEmptyEntries)
+        |> Seq.mapi (fun i sourceName ->
+            let path = getPath sourceName
+            
+            let application = Assembly.LoadFile path.FullName
+            
+            let types = application.GetExportedTypes ()
+            
+            let testTypes =
+                types
+                |> Array.map (fun t ->
+                    let properties =
+                        t.GetProperties (BindingFlags.Public ||| BindingFlags.Static)
+                    //     |> Array.filter (fun p ->
+                    //         let propInterfaces =
+                    //             p.PropertyType.GetInterfaces ()
+                    //         
+                    //         propInterfaces
+                    //         |> Array.contains typeof<ITest>
+                    //     )
+                    //     
+                    // 0 < properties.Length
+                    properties
+                )
+                |> Array.concat
+            
+            // let fns =
+            //     sourceName.Split ([|"."; "\\"|], StringSplitOptions.RemoveEmptyEntries)
                 
-            fns
-            |> Seq.map (fun fn -> TestCase ( $"%s{fn}..Test%d{i}", ExecutorUri |> Uri, $"%s{fn}..Test%d{i}")) 
+            // fns
+            // |> Seq.map (fun fn -> TestCase ( $"%s{fn}..Test%d{i}", ExecutorUri |> Uri, sourceName ))
+        
+            testTypes
+            |> Array.map (fun tt ->
+                TestCase ($"%s{tt.DeclaringType.FullName}.%s{tt.Name}", ExecutorUri |> Uri, sourceName)
+            )
             
         )
         |> Seq.concat
