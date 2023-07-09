ddbd202f34c8bc343085df7c6db373e725bd13f4
jason-kerney <jason@teamkerney.com>
2023-07-09 09:25:45 -0700

    feat: setup some constants

---
+++ TestAdapter/Setup.fs
@@ -0,0 +1,18 @@
+﻿[<Microsoft.FSharp.Core.AutoOpen>]
+module Archer.Quiver.TestAdapter.Setup
+
+open System
+open System.IO
+
+[<Literal>]
+let ExecutorUri = "executor://ArcherQuiverExecutor"
+
+let forbiddenDirectories =
+    [
+        Environment.GetEnvironmentVariable "ProgramW6432"
+        Environment.GetEnvironmentVariable "ProgramFiles(x86)"
+        Environment.GetEnvironmentVariable "ProgramFiles"
+        Environment.GetEnvironmentVariable "windir"
+    ]
+    |> List.filter (fun path -> String.IsNullOrEmpty path |> not)
+    |> List.map DirectoryInfo
