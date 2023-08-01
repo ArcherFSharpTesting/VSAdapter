e456159e464819a5635fdda7bd314eae38f667e0
jason-kerney <jason@teamkerney.com>
2023-08-01 12:48:21 -0700

    test: fixed failing test

--- Tests/FileLoader Should.fs
+++ Tests/FileLoader Should.fs
@@ -9,6 +9,7 @@ open Archer.Quiver.TestAdapter.FileWrappers
 type private Builders = {
     PathBuilder: bool -> IPathWrapper
     DirBuilder: string -> IDirectoryWrapper
+    FileInfoBuilder: string -> IFileInfoWrapper
 }
 
 let private feature = Arrow.NewFeature (
@@ -24,7 +25,24 @@ let private feature = Arrow.NewFeature (
                 member _.GetCurrentDirectory () = currentDirectory |> DefaultDirectoryInfo :> IDirectoryInfoWrapper
             }
             
-        Ok { PathBuilder = pathFakeBuilder; DirBuilder = fakeDirHelperBuilder }
+        let fakeDirInfoBuilder (path: string) =
+            {new IDirectoryInfoWrapper with
+                member _.GetFiles _ = [||]
+                member _.FullName with get () = path
+                member _.Exists with get () = true
+            }
+            
+        let fakeFileInfoBuilder (path: string) =
+            {new IFileInfoWrapper with
+                member _.Directory with get () = fakeDirInfoBuilder @"C:\"
+                member _.FullName with get () = path
+            }
+            
+        Ok {
+            PathBuilder = pathFakeBuilder
+            DirBuilder = fakeDirHelperBuilder
+            FileInfoBuilder = fakeFileInfoBuilder
+        }
     )
 )
 
@@ -34,7 +52,7 @@ let ``Return a rooted File`` =
             let dir = a.DirBuilder @"B:\bad\dir"
             let path = a.PathBuilder true
             
-            let loader = FileLoader ("myFile.dll", path, dir, fun s -> DefaultFileInfo s) :> IAssemblyLocator
+            let loader = FileLoader ("myFile.dll", path, dir, a.FileInfoBuilder) :> IAssemblyLocator
             
             let f = loader.GetPossibleTestFile ()
             f.FullName
